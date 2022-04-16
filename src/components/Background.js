54a8ec038ac3374028c70ccba17d094126cf99ee
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-04-15 20:04:06 -0700

    added topbar and list components

---
+++ src/components/Background.js
@@ -0,0 +1,10 @@
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+
+export default function Background() {
+    return (
+        <div style={{ width: '1300px' }}>
+
+        </div>
+    )
+}
